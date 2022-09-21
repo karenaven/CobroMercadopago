9145c3f6a94be127895ba5991e5c357a44face5b
karenaven <kdma37@hotmail.com>
2022-09-21 10:02:06 -0300

    202221091001

--- CobrarMercadoPago/CobrarMercadoPago/Form1.Designer.cs
+++ CobrarMercadoPago/CobrarMercadoPago/Form1.Designer.cs
@@ -28,12 +28,299 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button10 = new System.Windows.Forms.Button();
+            this.panelTeclado = new System.Windows.Forms.Panel();
+            this.textBoxMonto = new System.Windows.Forms.TextBox();
+            this.buttonBorrarTodo = new System.Windows.Forms.Button();
+            this.buttonBorrar = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2EscaneaQr = new System.Windows.Forms.Panel();
+            this.pictureBoxQR = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panelTeclado.SuspendLayout();
+            this.panel2EscaneaQr.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.panelTeclado);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1318, 764);
+            this.panel1.TabIndex = 0;
+            // 
+            // button10
+            // 
+            this.button10.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button10.BackColor = System.Drawing.Color.Chocolate;
+            this.button10.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button10.Location = new System.Drawing.Point(584, 626);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(201, 74);
+            this.button10.TabIndex = 2;
+            this.button10.Text = "ACEPTAR";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // panelTeclado
+            // 
+            this.panelTeclado.Controls.Add(this.textBoxMonto);
+            this.panelTeclado.Controls.Add(this.buttonBorrarTodo);
+            this.panelTeclado.Controls.Add(this.buttonBorrar);
+            this.panelTeclado.Controls.Add(this.button0);
+            this.panelTeclado.Controls.Add(this.button9);
+            this.panelTeclado.Controls.Add(this.button8);
+            this.panelTeclado.Controls.Add(this.button7);
+            this.panelTeclado.Controls.Add(this.button6);
+            this.panelTeclado.Controls.Add(this.button5);
+            this.panelTeclado.Controls.Add(this.button4);
+            this.panelTeclado.Controls.Add(this.button3);
+            this.panelTeclado.Controls.Add(this.button2);
+            this.panelTeclado.Controls.Add(this.button1);
+            this.panelTeclado.Location = new System.Drawing.Point(512, 157);
+            this.panelTeclado.Name = "panelTeclado";
+            this.panelTeclado.Size = new System.Drawing.Size(330, 432);
+            this.panelTeclado.TabIndex = 1;
+            // 
+            // textBoxMonto
+            // 
+            this.textBoxMonto.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxMonto.Location = new System.Drawing.Point(53, 30);
+            this.textBoxMonto.Name = "textBoxMonto";
+            this.textBoxMonto.Size = new System.Drawing.Size(230, 50);
+            this.textBoxMonto.TabIndex = 3;
+            // 
+            // buttonBorrarTodo
+            // 
+            this.buttonBorrarTodo.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonBorrarTodo.Location = new System.Drawing.Point(216, 344);
+            this.buttonBorrarTodo.Name = "buttonBorrarTodo";
+            this.buttonBorrarTodo.Size = new System.Drawing.Size(68, 62);
+            this.buttonBorrarTodo.TabIndex = 12;
+            this.buttonBorrarTodo.Text = "C";
+            this.buttonBorrarTodo.UseVisualStyleBackColor = true;
+            this.buttonBorrarTodo.Click += new System.EventHandler(this.buttonBorrarTodo_Click);
+            // 
+            // buttonBorrar
+            // 
+            this.buttonBorrar.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonBorrar.Location = new System.Drawing.Point(53, 344);
+            this.buttonBorrar.Name = "buttonBorrar";
+            this.buttonBorrar.Size = new System.Drawing.Size(68, 62);
+            this.buttonBorrar.TabIndex = 11;
+            this.buttonBorrar.Text = "<-";
+            this.buttonBorrar.UseVisualStyleBackColor = true;
+            this.buttonBorrar.Click += new System.EventHandler(this.buttonBorrar_Click);
+            // 
+            // button0
+            // 
+            this.button0.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button0.Location = new System.Drawing.Point(136, 344);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(68, 62);
+            this.button0.TabIndex = 10;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button9.Location = new System.Drawing.Point(215, 264);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(68, 62);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button8.Location = new System.Drawing.Point(136, 264);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(68, 62);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(53, 264);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(68, 62);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(215, 185);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(68, 62);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(136, 185);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(68, 62);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(53, 185);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(68, 62);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(215, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(68, 62);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(136, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 62);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Emoji", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(53, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 62);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Chocolate;
+            this.label1.Location = new System.Drawing.Point(445, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(466, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Monto a Pagar";
+            // 
+            // panel2EscaneaQr
+            // 
+            this.panel2EscaneaQr.Controls.Add(this.pictureBoxQR);
+            this.panel2EscaneaQr.Controls.Add(this.label2);
+            this.panel2EscaneaQr.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2EscaneaQr.Location = new System.Drawing.Point(0, 0);
+            this.panel2EscaneaQr.Name = "panel2EscaneaQr";
+            this.panel2EscaneaQr.Size = new System.Drawing.Size(1318, 764);
+            this.panel2EscaneaQr.TabIndex = 1;
+            // 
+            // pictureBoxQR
+            // 
+            this.pictureBoxQR.Location = new System.Drawing.Point(506, 258);
+            this.pictureBoxQR.Name = "pictureBoxQR";
+            this.pictureBoxQR.Size = new System.Drawing.Size(371, 374);
+            this.pictureBoxQR.TabIndex = 1;
+            this.pictureBoxQR.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 63F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Chocolate;
+            this.label2.Location = new System.Drawing.Point(408, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(600, 95);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "ESCANEA QR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1318, 764);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2EscaneaQr);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panelTeclado.ResumeLayout(false);
+            this.panelTeclado.PerformLayout();
+            this.panel2EscaneaQr.ResumeLayout(false);
+            this.panel2EscaneaQr.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQR)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panelTeclado;
+        private Button buttonBorrarTodo;
+        private Button buttonBorrar;
+        private Button button0;
+        private Button button9;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label1;
+        private Button button10;
+        private Panel panel2EscaneaQr;
+        private Label label2;
+        private PictureBox pictureBoxQR;
+        private TextBox textBoxMonto;
     }
 }
--- CobrarMercadoPago/CobrarMercadoPago/Form1.cs
+++ CobrarMercadoPago/CobrarMercadoPago/Form1.cs
@@ -1,10 +1,135 @@
+using MercadoPagoPaymentQR.Clases;
+
 namespace CobrarMercadoPago
 {
     public partial class Form1 : Form
     {
+        private string orden_reference;
         public Form1()
         {
             InitializeComponent();
         }
+        
+        #region TECLADO
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxMonto.Text+= "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxMonto.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBoxMonto.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBoxMonto.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBoxMonto.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBoxMonto.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBoxMonto.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBoxMonto.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBoxMonto.Text += "9";
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            textBoxMonto.Text += "0";
+        }
+
+        private void buttonBorrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBoxMonto.Text = textBoxMonto.Text.Substring(0, textBoxMonto.Text.Length - 1);
+            }
+            catch (Exception) { }
+        }
+
+        private void buttonBorrarTodo_Click(object sender, EventArgs e)
+        {
+            textBoxMonto.Text = "";
+        }
+
+
+
+        #endregion
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            panel1.Visible=false;
+            panelTeclado.Visible = false;
+            panel2EscaneaQr.Visible = true;
+            iniciarMercadoPago();
+
+        }
+
+        private async void iniciarMercadoPago()
+        {
+            HttpClient client = new HttpClient();
+            string accessesToken = "APP_USR-7156108107310509-090909-5435f39621de9ec69896d4beeb6b6041-1195179584";
+            string idVendedor = "1195179584";
+            MercadoPagoPaymentQR.MercadoPagoAPI mercadopago = new MercadoPagoPaymentQR.MercadoPagoAPI(client, accessesToken);
+            Orden.Item Item = new Orden.Item();
+            Item.sku_number = "COOLPAY2022";
+            Item.category = "TU PAGO EN COOLPAY";
+            Item.title = "TU PAGO EN COOLPAY";
+            Item.description = "";
+            Item.unit_price = Convert.ToInt32(textBoxMonto.Text);
+            Item.quantity = 1;
+            Item.unit_measure = "unit";
+            Item.total_amount = Convert.ToInt32(textBoxMonto.Text);
+
+            List<Orden.Item> items = new List<Orden.Item>();
+            items.Add(Item);
+
+            Orden orden = new Orden();
+            orden.external_reference = generarExternalID();
+            orden_reference = orden.external_reference;
+            orden.title = "TU PAGO EN COOLPAY";
+            orden.description = "";
+            orden.notification_url = "https://www.yourserver.com/notifications";
+            orden.expiration_date = DateTime.Now.AddDays(30).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss.fffzzz");
+            orden.total_amount = Convert.ToInt32(textBoxMonto.Text);
+            orden.items = items;
+
+
+            string qr = await mercadopago.CrearOrdenPOS(orden, idVendedor, "SUC002POST003");
+            Console.WriteLine("////////////////////////////////");
+            Console.WriteLine(qr);
+            pictureBoxQR.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxQR.Image = mercadopago.TranformarStringToQR(qr);
+        }
+
+        private string generarExternalID()
+        {
+            string externalId = "Order-" + DateTime.Now.ToString("yyyyMMddHHmmss");
+            return externalId;
+        }
+
     }
 }
